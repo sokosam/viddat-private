6a9933bef5de852f5fd8785d63bca32b47926d8b
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2024-06-28 19:52:42 -0400

    everything i worked on for the past weeks

---
+++ app/auth.py
@@ -0,0 +1,97 @@
+from flask_mail import Mail, Message
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
+from models import User
+from __init__ import db, mailer
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+
+auth = Blueprint('auth', __name__)
+
+s = URLSafeTimedSerializer('testSecret')
+
+@auth.route('/login', methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('client_page.client'))  
+
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email = email).first()
+        if user:
+            if check_password_hash(user.password, password) and user.emailConfirm:
+                login_user(user, remember=True)
+                session['email'] = email
+                return redirect(url_for('client_page.client'))
+            else:
+                flash('Wrong email or password!', category='error')
+        else:
+            flash("No such email registered!", category='error')
+    return render_template('logIn.html', user=current_user)
+
+
+@auth.route('/logout', methods=["GET","POST"])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
+
+
+@auth.route('/sign_up',methods=["GET","POST"])
+def sign_up():
+    if current_user.is_authenticated:
+        return redirect(url_for('client_page.client'))  
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get('password')
+        password_confirm = request.form.get('confirm')
+
+        user = User.query.filter_by(email=email).first()
+        token = s.dumps(email,  salt='email_confirm')
+
+        if user:
+            if not user.emailConfirm:
+                link = url_for('auth.confirm_email', token=token, _external=True)
+                msg = Message(subject='Confirm Your Account', body=f'Please confirm your email: ' +link ,sender="viddatconfirm@gmail.com",recipients=[email])
+                mailer.send(msg)
+                return redirect(url_for("auth.verifyEmail"))
+            else:
+                    flash("An account is already registered with this email!", category="error")
+        elif len(email) < 5:
+            flash("Email must be atleast 6 characters!", category="error")
+        elif len(password) < 7:
+            flash("Password must be atleast 8 characters!", category="error")
+        elif password != password_confirm:
+            flash("Passwords do not match!", category="error")
+        else:
+            new_user = User(email=email, emailConfirm=False, password = generate_password_hash(password, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            link = url_for('auth.confirm_email', token=token, _external=True)
+            msg = Message(subject='Confirm Your Account', body=f'Please confirm your email: ' +link ,sender="viddatconfirm@gmail.com",recipients=[email])
+            mailer.send(msg)
+            flash("Account registered", category="success!")
+            return redirect(url_for("auth.verifyEmail"))
+
+    return render_template("signUp.html", user=current_user)
+
+
+@auth.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        email = s.loads(token, salt='email_confirm', max_age=30)
+    except SignatureExpired:
+        return render_template("emailExpired.html")
+    user = User.query.filter_by(email = email).first()
+    user.emailConfirm = True
+    db.session.commit()
+    login_user(user, remember=True)
+    session['email'] = email
+    return render_template('emailVerified.html', user=current_user)
+
+@auth.route('/confirm_email/verify')
+def verifyEmail():
+    return  render_template('checkEmail.html')
---
+++ worker/text_scripts.py
@@ -0,0 +1,82 @@
+from unidecode import unidecode
+import regex
+import re 
+
+def pollySplicer(text):
+    textList = []
+    stops = (".","!", "?"," ")
+    count = 0
+    curr = ""
+    for char in text:
+        if count > 2000 and char in stops:
+            curr = curr + char
+            textList.append(curr)
+            curr = ""
+            count = 0
+            continue
+        count+=1
+        curr = curr + char
+    textList.append(curr)
+    return textList
+
+def reduce_repeated_punctuation(text):
+    # Replace consecutive periods with a single period
+    text = re.sub(r'\.{2,}', '.', text)
+
+    text = re.sub(r'(\d+)-(\d+)', r'\1 to \2', text)
+
+    # Replace consecutive question marks with a single question mark
+    text = re.sub(r'\?{2,}', '?', text)
+
+    # Replace consecutive exclamation marks with a single exclamation mark
+    text = re.sub(r'!{2,}', '!', text)
+    
+    text = re.sub(r'&{2,}', ' and ', text)
+    text = re.sub(r'&', ' and ', text)
+
+    return text
+
+def getGender(text):
+    # male = len( regex.findall( r"(?<=(me|I|myself)\s*)\((\d{2}[mM])\)", text, regex.IGNORECASE))
+    male = len( regex.findall( r"((?<=(me|I|myself|my)\s*)\(?\s*(\d{2}[mM])\)?)", text, regex.IGNORECASE))
+    female = len(regex.findall(r"((?<=(me|I|myself|my)\s*)\(?\s*(\d{2}[fF])\)?)", text, regex.IGNORECASE))
+    if male > 0: return 999999
+    if female > 0: return -999999
+    checker = regex.findall(r"\bmy\s+.*?(wife|girlfriend)\b", text, regex.IGNORECASE)
+    for i in checker: 
+        if i[-10:].lower() == 'girlfriend':
+            if len(i) < 20:
+                male+=1
+        elif i[-4:].lower() == 'wife':
+            if len(i) <16:
+                male+=1
+    checker = regex.findall(r"\bmy\s+.*?(boyfriend|husband)\b", text, regex.IGNORECASE)
+    for i in checker: 
+        if i[-9:].lower() == 'boyfriend':
+            if len(i) < 19:
+                female+=1
+        elif i[-7:].lower() == 'husband':
+            if len(i) <18:
+                female+=1    
+    return male-female
+
+# Deprecated
+# def changeAcro(txt):
+#     acronyms = {}
+#     if txt.lower() in acronyms or re.sub(r'[^a-zA-Z]', "",txt.lower()) in acronyms:
+#         if txt.lower() in acronyms: return acronyms[txt.lower()]
+#         else: return  re.sub(re.sub(r'[^a-zA-Z]', "",txt.lower()), acronyms[re.sub(r'[^a-zA-Z]', "",txt.lower())] ,txt.lower())
+#     else: return txt
+
+# Deprecated
+# def getTextFromFile(file):
+#     txtFile = open(file, encoding='utf-8', errors='ignore', mode='r')
+#     raw = ""
+#     for i in txtFile:
+#         for k in i.split(" "):
+#             for z in k: 
+#                 z = changeAcro(z)
+#             if k == "-" or k== "(" or k==")" :
+#                 raw += changeAcro(k)
+#             else: raw += " " + changeAcro(k)
+#     return unidecode(reduce_repeated_punctuation(raw.replace('“', '"').replace('”', '"').replace("’", "'")))       
