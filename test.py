2d47a28533c8aa10cf2c0265cc18f1c961494b96
Samuel Shi <samuelshiwork@gmail.com>
2024-07-27 17:03:05 -0400

    getting ready for production

--- test.py
+++ test.py
@@ -14,5 +14,6 @@
 # test.generate_video(text=params['TEXT'], title=params['TITLE'], red_text=params['PART'], output_path="temp"+params["ID"], output_name="video.mp4",stock_footage=r"C:\Users\Samue\Documents\GitHub\viddat\worker\stock_footage\cooking.mp4")
 
 
-from worker.title_page_gen import thumbnail_generator
-thumbnail_generator(text="hi", output_path="", url=r"https://tsbckt.s3.amazonaws.com/pfp/7WiYW4MZ2VjSZqZseoF4ajIkI1.png")
+# from worker.title_page_gen import thumbnail_generator
+# thumbnail_generator(text="hi", output_path="", url=r"https://tsbckt.s3.amazonaws.com/pfp/7WiYW4MZ2VjSZqZseoF4ajIkI1.png")
+
