aa2af0f684fd13eb60aee0036e357c71bf9cffde
Samuel Shi <samuelshiwork@gmail.com>
2024-07-27 18:51:48 -0400

    email fix

--- app/__init__.py
+++ app/__init__.py
@@ -14,11 +14,11 @@ from flask_socketio import SocketIO
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465 
+app.config['MAIL_PORT'] = 587 
 app.config['MAIL_USERNAME'] = 'viddatconfirm@gmail.com'
 app.config['MAIL_PASSWORD'] = getEmailPWD()
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
 
 # import logging
